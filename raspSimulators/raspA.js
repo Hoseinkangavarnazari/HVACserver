0c8b01bf3d439d5928704a268efb1653bad45bb7
AmirHossein Nazari <hnirsnazari@yahoo.com>
2019-07-12 21:06:23 +0430

    connection between simulated raspberry pi and server

---
+++ raspSimulators/raspA.js
@@ -0,0 +1,21 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+const port = 4001;
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+app.use(bodyParser.json());
+
+app.post('/', (req,res)=>{
+    console.log(req.body.PM)
+    res.send("Received your packet successfully : RASP-A");
+})
+
+
+
+
+app.listen(port, () => console.log(`::: Example app listening on port ${port}!`))
---
+++ raspSimulators/raspB.js
@@ -0,0 +1,21 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+const port = 4002;
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+app.use(bodyParser.json());
+
+app.post('/', (req,res)=>{
+    console.log(req.body.PM)
+    res.send("Received your packet successfully : RASP-B");
+})
+
+
+
+
+app.listen(port, () => console.log(`::: Example app listening on port ${port}!`))
