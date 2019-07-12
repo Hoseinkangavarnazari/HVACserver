ae485c95e45e6f9f9caa8a87788d2f8ca8b33c21
AmirHossein Nazari <hnirsnazari@yahoo.com>
2019-07-12 21:33:58 +0430

    adding html testing platform

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require('express')
-var bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require('express');
+var bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const serverRouter= express.Router();
+
+serverRouter.get('/', (req, res) => {
+  console.log("received a request from browser");
+  res.sendFile(path.join(__dirname + '/test.html'));
+}
+)
+
+app.use('/page', serverRouter);
 
 
 app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  extended: true
+}));
 
 app.use(bodyParser.json());
 
@@ -15,6 +26,8 @@ app.use(bodyParser.json());
 
 var node_route = require('./routes/node.routes');
 app.use('/node', node_route);
+
+
 
 
 app.listen(port, () => console.log(`::: Example app listening on port ${port}!`))
